92a33ed3a03ca803d508dac05f6e8af9a8f5f91f
karpukhinp01 <88552184+karpukhinp01@users.noreply.github.com>
2023-02-16 01:01:56 +0100

    favorites

--- app/src/main/java/com/example/upstreamboredapi/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/upstreamboredapi/viewmodel/BaseViewModel.kt
@@ -7,14 +7,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
+// Define an abstract class named BaseViewModel that extends AndroidViewModel and implements CoroutineScope
 abstract class BaseViewModel(Application: Application) : AndroidViewModel(Application),
     CoroutineScope {
 
+    // Create a Job instance
     private val job = Job()
 
+    // Override the coroutineContext property to return the job and Dispatchers.Main
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.Main
 
+    // Override the onCleared() method to cancel the job
     override fun onCleared() {
         super.onCleared()
         job.cancel()
